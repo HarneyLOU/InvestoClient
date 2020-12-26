53417dc2386c743e973cd3f2db9c8184b1ceb820
HarneyLOU <inharnfer@gmail.com>
2020-12-26 21:31:58 +0100

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://investoapiweb20201226194228.azurewebsites.net/api',
+  websocketUrl: 'https://investoapiweb20201226194228.azurewebsites.net',
 };
